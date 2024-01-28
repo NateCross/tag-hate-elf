340bc8d4acf0c85a215a364716b5997cd6cf169a
mebzmoren <ellyzajpapas@su.edu.ph>
2024-01-28 13:39:37 +0800

    added comments

--- scraping/pushshift-scraping.py
+++ scraping/pushshift-scraping.py
@@ -1,9 +1,13 @@
-from pmaw import PushshiftAPI
+# Import necessary modules
+from pmaw import PushshiftAPI   # Used to access Reddit data
 
+# Allows us to interact with the API to fetch Reddit data & returns  a generator of comment objects that match the search criteria
 api = PushshiftAPI()
 comments = api.search_comments(
-    subreddit="philippines",
-    limit=1000,
+    subreddit="philippines",    # Specifies the subreddit where comments will be fetched
+    limit=1000, # Limits the number of comments fetched
 )
+
+# Iterate over each comment objext in the comments generator & prints them
 for comment in comments:
     print(comment)
--- scraping/utils.py
+++ scraping/utils.py
@@ -1,27 +1,32 @@
 """
 Credit: https://stackoverflow.com/questions/761824/python-how-to-convert-markdown-formatted-text-to-text
 """
-from markdown import Markdown
-from io import StringIO
 
 
+# Import necessary modules
+from markdown import Markdown   # Handles markdown text
+from io import StringIO # For in-memory text stream
+
+
+# Function to recursively convert markdown elements to plain text
 def unmark_element(element, stream=None):
     if stream is None:
-        stream = StringIO()
+        stream = StringIO() # Create a new in-memory text stream if none is provided
     if element.text:
-        stream.write(element.text)
+        stream.write(element.text)  # Write the current element's text to the stream
     for sub in element:
-        unmark_element(sub, stream)
+        unmark_element(sub, stream) # Recursively process all child elements
     if element.tail:
-        stream.write(element.tail)
-    return stream.getvalue()
+        stream.write(element.tail)  # Write the tail text (text after this element) to the stream
+    return stream.getvalue()    # Return the entire content of the stream as a string
 
 
-# patching Markdown
-Markdown.output_formats["plain"] = unmark_element
-__md = Markdown(output_format="plain")
-__md.stripTopLevelTags = False
+# patching Markdown class to support plain text output
+Markdown.output_formats["plain"] = unmark_element   # Add a new output format 'plain' to the Markdown class, using the unmark_element function
+__md = Markdown(output_format="plain")  # Create a Markdown instance configured to use the 'plain' output format
+__md.stripTopLevelTags = False # Configure the Markdown instance not to strip top-level tags
 
 
+# Function to convert markdown text to plain text
 def unmark(text):
     return __md.convert(text)
