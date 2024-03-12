16e85cc538b20062f15b483e317bd3885894f754
Nathan Angelo B. Cruz <nate.a.b.cross@gmail.com>
2024-03-12 19:23:16 +0800

    Change clean training to work on folders

--- clean-training.py
+++ clean-training.py
@@ -1,9 +1,9 @@
 import shutil
 try:
-    shutil.rmtree("train_lstm")
-    shutil.rmtree("train_bert")
+    shutil.rmtree("model_lstm")
+    shutil.rmtree("model_bert")
 except FileNotFoundError:
-    print("ERROR: The folders 'train_lstm' or 'train_bert' were not found.")
+    print("ERROR: The folders 'model_lstm' or 'model_bert' were not found.")
     print("Please finish training first before cleaning.")
     exit(1)
 print("Cleaned training checkpoints")
